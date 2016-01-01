799a0898bb2e413030115dfcff97588011824647
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2016-01-01 17:11:29 +0600

    Asynchronous mocha testing

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -1,9 +1,23 @@
 const expect = require('chai').expect;
 const tools = require('../lib/tools');
 
-describe('printName()', () => {
-    it('should print the last name first', () => {
-        var results = tools.printName({first: 'Tahmid', last: 'Tanzim'});
-        expect(results).to.equal('Tanzim, Tahmid');
+describe('Tools', () => {
+
+    describe('printName()', () => {
+        it('should print the last name first', () => {
+            var results = tools.printName({first: 'Tahmid', last: 'Tanzim'});
+            expect(results).to.equal('Tanzim, Tahmid');
+        });
+    });
+
+    describe('loadWiki()', function() {
+        this.timeout(10000);
+        it("Load Abraham Lincoln's wikipidia page", done => {
+            tools.loadWiki({first: 'Abraham', last: 'Lincoln'}, html => {
+                expect(html).to.be.ok;
+                done();
+            });
+        });
     });
 });
+
